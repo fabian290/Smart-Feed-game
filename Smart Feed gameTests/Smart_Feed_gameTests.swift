4ee0d485c92d731d8133d83fe22ee89aa1392f9b
Fabian Hernandez Aguilar <fhaguilar2019@208-Engineering-17.local>
2019-04-11 13:18:20 -0700

    Initial Commit

---
+++ Smart Feed gameTests/Smart_Feed_gameTests.swift
@@ -0,0 +1,34 @@
+//
+//  Smart_Feed_gameTests.swift
+//  Smart Feed gameTests
+//
+//  Created by Fabian Hernandez Aguilar on 4/11/19.
+//  Copyright © 2019 Fabian Hernandez Aguilar. All rights reserved.
+//
+
+import XCTest
+@testable import Smart_Feed_game
+
+class Smart_Feed_gameTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
